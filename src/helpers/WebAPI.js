add3279f5316f4bc421eb6d591871f2ffd1c67ec
Vincent Michael <ma20200415@gmail.com>
2022-03-16 01:53:47 +0800

    create API

---
+++ src/helpers/WebAPI.js
@@ -0,0 +1,16 @@
+import Config from '../config.json'
+
+export const signin = async (data = {}) => {
+    return await fetch(`${Config.BASE_URL}/signin`, {
+        method: "POST",
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(
+        (res) => res.json(),
+        (error) => { console.log(error) }
+    );
+};
+
