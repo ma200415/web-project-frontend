d2fa5ba3491ea95d03b7999bf816d168afd81aef
Vincent Michael <ma20200415@gmail.com>
2022-04-17 23:25:58 +0800

    add some helpers

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -7,3 +7,30 @@ export const setAuthToken = (authToken) => {
 export const getAuthToken = () => {
     return localStorage.getItem(AUTHTOKEN_NAME);
 };
+
+export const getUserName = (firstName, lastName) => {
+    return firstName + " " + lastName
+}
+
+export const getDogAge = (date) => {
+    return new Date().getFullYear() - new Date(date).getFullYear()
+}
+
+export const getGender = (gender) => {
+    switch (gender) {
+        case "m":
+            return "Male"
+        case "f":
+            return "Female"
+        default:
+            return gender
+    }
+}
+
+export const dateToString = (date) => {
+    return (date && new Date(date).toLocaleString())
+}
+
+export const minMaxDateFormat = (date) => {
+    return date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + date.getDate().toString().padStart(2, "0")
+}
